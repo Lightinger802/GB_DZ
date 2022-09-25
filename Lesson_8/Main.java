6714aa863501b8da5a71130fd1c75a5510022c83
Lightinger <Lightinger802@gmail.com>
2022-09-25 12:36:00 +0300

    DZ_Lesson_8

---
+++ Lesson_8/Main.java
@@ -0,0 +1,11 @@
+package Lesson_8;
+
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        new ApplicationForm("Calculator v 1.0");
+
+
+    }
+}
---
+++ Lesson_8/components/OperatorJButton.java
@@ -0,0 +1,12 @@
+package Lesson_8.components;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class OperatorJButton extends JButton {
+    public OperatorJButton(String text) {
+        super(text);
+        setFont(new Font("TimesRoman", Font.PLAIN, 30));
+        setBackground(new Color(224, 151, 14));
+    }
+}
